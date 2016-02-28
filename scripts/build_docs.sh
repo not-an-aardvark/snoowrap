f933e6d9a76644b64d927052c89a009278b4a84f
not-an-aardvark <not-an-aardvark@users.noreply.github.com>
2016-02-27 19:50:34 -0500

    Silence the buildscript commit
    [ci skip]

--- scripts/build_docs.sh
+++ scripts/build_docs.sh
@@ -8,5 +8,5 @@ git init
 git config user.name "not-an-aardvark"
 git config user.email "not-an-aardvark@users.noreply.github.com"
 git add .
-git commit -m "Docs for commit $TRAVIS_COMMIT"
+git commit -qm "Docs for commit $TRAVIS_COMMIT"
 git push -fq "https://${GH_TOKEN}@${GH_REF}" master:gh-pages > /dev/null 2>&1
