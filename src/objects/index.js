1bffce2a2512cf07bf43abf2c121c174468a40f6
not-an-aardvark <not-an-aardvark@users.noreply.github.com>
2016-04-23 16:23:43 -0400

    Make the formatting in objects/index.js consistent with everything else

--- src/objects/index.js
+++ src/objects/index.js
@@ -1,16 +1,17 @@
 'use strict';
-// These could be used in a require('fs') loop rather than being explicitly listed, but that would cause browserify issues
-exports.RedditContent = require('./RedditContent');
-exports.ReplyableContent = require('./ReplyableContent');
-exports.VoteableContent = require('./VoteableContent');
-exports.Comment = require('./Comment');
-exports.RedditUser = require('./RedditUser');
-exports.Submission = require('./Submission');
-exports.LiveThread = require('./LiveThread');
-exports.PrivateMessage = require('./PrivateMessage');
-exports.Subreddit = require('./Subreddit');
-exports.MultiReddit = require('./MultiReddit');
-exports.WikiPage = require('./WikiPage');
-exports.Listing = require('./Listing');
-exports.More = require('./More');
-exports.UserList = require('./UserList');
+module.exports = {
+  RedditContent: require('./RedditContent'),
+  ReplyableContent: require('./ReplyableContent'),
+  VoteableContent: require('./VoteableContent'),
+  Comment: require('./Comment'),
+  RedditUser: require('./RedditUser'),
+  Submission: require('./Submission'),
+  LiveThread: require('./LiveThread'),
+  PrivateMessage: require('./PrivateMessage'),
+  Subreddit: require('./Subreddit'),
+  MultiReddit: require('./MultiReddit'),
+  WikiPage: require('./WikiPage'),
+  Listing: require('./Listing'),
+  More: require('./More'),
+  UserList: require('./UserList')
+};
