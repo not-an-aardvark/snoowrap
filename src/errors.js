d23960af29b0c8d6064ca4d48ca12e38f058faa1
not-an-aardvark <not-an-aardvark@users.noreply.github.com>
2016-04-30 00:53:43 -0400

    Make the ratelimit warning a bit more readable

--- src/errors.js
+++ src/errors.js
@@ -31,5 +31,5 @@ module.exports = {
   InvalidMethodCallError: sub_error('InvalidMethodCallError', ''),
   NoCredentialsError: sub_error('NoCredentialsError', `Missing credentials passed to ${constants.MODULE_NAME} constructor. You must pass an object containing either (a) user_agent, client_id, client_secret, and refresh_token properties, or (b) user_agent and access_token properties. For information, please read the docs at ${constants.DOCS_LINK}.`),
   MissingUserAgentError: sub_error('MissingUserAgentError', `You must supply an object with the user_agent property to the snoowrap constructor. For more details on user_agent strings, please see: ${constants.API_RULES_LINK}`),
-  RateLimitWarning: time_until_reset => `Warning: ${constants.MODULE_NAME} temporarily stopped sending requests because reddit's ratelimit was exceeded. The request you attempted to send was queued, and will be sent to reddit when the current ratelimit period expires in ${time_until_reset} milliseconds.`
+  RateLimitWarning: milliseconds_until_reset => `Warning: ${constants.MODULE_NAME} temporarily stopped sending requests because reddit's ratelimit was exceeded. The request you attempted to send was queued, and will be sent to reddit when the current ratelimit period expires in ${milliseconds_until_reset / 1000} seconds.`
 };
