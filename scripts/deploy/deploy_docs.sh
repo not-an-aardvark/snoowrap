71bc448c03b5a053f02fc44d22d2a1cea6fbd501
Teddy Katz <teddy.katz@gmail.com>
2016-11-15 01:19:08 -0500

    Fix paths in the deploy script
    [ci skip]

--- scripts/deploy/deploy_docs.sh
+++ scripts/deploy/deploy_docs.sh
@@ -13,10 +13,10 @@ ENCRYPTED_KEY_VAR="encrypted_${ENCRYPTION_LABEL}_key"
 ENCRYPTED_IV_VAR="encrypted_${ENCRYPTION_LABEL}_iv"
 ENCRYPTED_KEY=${!ENCRYPTED_KEY_VAR}
 ENCRYPTED_IV=${!ENCRYPTED_IV_VAR}
-openssl aes-256-cbc -K $ENCRYPTED_KEY -iv $ENCRYPTED_IV -in scripts/deploy/deploy_key.enc -out scripts/deploy/deploy_key -d
+openssl aes-256-cbc -K $ENCRYPTED_KEY -iv $ENCRYPTED_IV -in ./deploy_key.enc -out ./deploy_key -d
 chmod 600 deploy_key
 eval `ssh-agent -s`
-ssh-add scripts/deploy/deploy_key
+ssh-add ./deploy_key
 cd ../..
 
 rm -rf doc/ || true
