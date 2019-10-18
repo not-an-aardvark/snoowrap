bbc5f65479ac1acebf86f1ce4eb06f7ac451a9ca
Teddy Katz <teddy.katz@gmail.com>
2019-10-18 17:28:23 -0400

    Add release instructions (#223)
    * Added CircleCI for building and linting (#222)
    
    * Fix indentation
    
    * Fix deploy script for local running
    
    * Add release instructions
    
    * Add pushing-to-github instructions

--- scripts/deploy/deploy_docs.sh
+++ scripts/deploy/deploy_docs.sh
@@ -3,8 +3,6 @@
 set -e
 
 REPO=`git config remote.origin.url`
-SSH_REPO=${REPO/https:\/\/github.com\//git@github.com:}
-
 rm -rf doc/ || true
 git clone $REPO --branch gh-pages --single-branch --depth 1 doc
 scripts/build_docs.sh
@@ -13,4 +11,4 @@ cd doc/
 git add -A
 git commit -m "Docs for v$(npm info .. version)"
 
-git push $SSH_REPO $TARGET_BRANCH
+git push origin gh-pages
