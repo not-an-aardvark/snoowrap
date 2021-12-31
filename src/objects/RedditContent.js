9f830666e40622bea0f0eaca9e494a7593466edf
Dimitry Vinogradov <dimitryvin@gmail.com>
2021-12-30 21:45:40 -0800

    Cleanup

--- src/objects/RedditContent.js
+++ src/objects/RedditContent.js
@@ -51,11 +51,12 @@ const RedditContent = class RedditContent {
   */
   fetch () {
     if (!this._fetch) {
+      let getParams = {uri: this._uri}
+      // a submission we want to sort comments with
       if (this.constructor._name === 'Submission' && this.sort) {
-        this._fetch = this._r._promiseWrap(this._r._get({uri: this._uri, qs: { sort: this.sort } }).then(res => this._transformApiResponse(res)));
-      } else {
-        this._fetch = this._r._promiseWrap(this._r._get({uri: this._uri}).then(res => this._transformApiResponse(res)));
+        getParams = {...getParams, qs: { sort: this.sort }}
       }
+      this._fetch = this._r._promiseWrap(this._r._get(getParams).then(res => this._transformApiResponse(res)));
     }
     return this._fetch;
   }
